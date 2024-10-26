ec1e3c9afca9acc6df93bce9f74496bc96a85e5f
Adam Erik Dominik <domi.adamerik@gmail.com>
2024-10-27 00:18:12 +0200

    5. feladat

---
+++ src/main/java/hu/petrik/szuperhosprojekt/Vasember.java
@@ -0,0 +1,29 @@
+package hu.petrik.szuperhosprojekt;
+
+import java.text.MessageFormat;
+import java.util.Random;
+
+public class Vasember extends Bosszuallo implements Milliardos{
+    public Vasember() {
+        super(150, true);
+    }
+
+    @Override
+    public void kutyutKeszit() {
+        Random random= new Random();
+        setSzuperero(getSzuperero()+random.nextDouble(10));
+    }
+
+    @Override
+    public boolean megmentiAVilagot() {
+        if(mekkoraAzEreje()>1000){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("Vasember: {0}", super.toString());
+    }
+}
